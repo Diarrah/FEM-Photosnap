6e1a5614cc283ae23aafc9743a8044f86fdafbfa
Diarrah <diarrahcasiano@gmail.com>
2020-09-10 14:00:16 -0700

    -

---
+++ src/images/features/index.js
@@ -0,0 +1,6 @@
+export { default as responsive } from './desktop/responsive.svg';
+export { default as noLimit } from './desktop/no-limit.svg';
+export { default as embeded } from './desktop/embed.svg';
+export { default as domain } from './desktop/custom-domain.svg';
+export { default as exposure } from './desktop/boost-exposure.svg';
+export { default as dragDrop } from './desktop/drag-drop.svg';
