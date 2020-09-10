992e54db4c29204cdd737e0ea545ec99e03413ce
Diarrah <diarrahcasiano@gmail.com>
2020-09-10 14:02:27 -0700

    Grouping for easy import

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+export { default as Navbar } from './Navbar';
+export { default as PageLinks } from './PageLinks';
+export { default as Footer } from './Footer';
+export { default as CTA } from './CTA';
+export { default as Button } from './Button';
+
+
+// Home
+export { default as Hero } from './Home/Hero';
+export { default as StoriesPgCTA } from './Home/StoriesPageRedirectCTA';
+export { default as StoriesPreviewBar } from './Home/StoriesPreviewBar';
+export { default as Attributes } from './Home/CompanyAttributes';
+
+
+// Stories
+export { default as StoriesHero } from './Stories/StoryHero';
+export { default as StoryThumbnail } from './Stories/StoryThumbnailLink';
+
+
+// Features
+export { default as FeatureHeader } from './Features/FeaturesHeader';
+export { default as FeaturesGrid } from './Features/FeaturesGrid';
+export { default as FeatureCard } from './Features/FeatureAttribute';
+
+
+// Pricing
+export { default as PricingHeader } from './Pricing/PricingHeader';
+export { default as PricingToggle } from './Pricing/PricingToggle';
+export { default as PriceCard } from './Pricing/PriceCard';
+export { default as PricingTable } from './Pricing/PricingTable';
---
+++ src/mainPages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from './Home';
+export { default as Stories } from './Stories';
+export { default as Features } from './Features';
+export { default as Pricing } from './Pricing';
