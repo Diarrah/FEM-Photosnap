bd64fa7c375871f915c5e89624f807fc77f2eaa5
Diarrah <diarrahcasiano@gmail.com>
2020-09-11 20:56:18 -0700

    Changing multiple classNames based on state change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import { PageLinks, Button } from './';
 import logo from '../images/shared/desktop/logo.svg';
@@ -6,66 +6,66 @@ import open from '../images/shared/mobile/menu.svg';
 import close from '../images/shared/mobile/close.svg';
 
 const Navbar = () => {
-    const [symbol, setSymbol] = useState(),
-          [overlay, setOverlay] = useState(),
-          [mobileMenu, setMobileMenu] = useState(),
-          { body } = document;
+    const [menuOpen, setMenuOpen] = useState(false);
+    const navDeployment = useRef();
+    const { body } = document;
 
     useEffect(() => {
-        setSymbol(document.querySelector('.navbar__mobile__btn'));
-        setOverlay(document.querySelector('.overlay'));
-        setMobileMenu(document.querySelector('.navbar__mobile__menu'));  
-    }, [])     
+        handleMobileNavClick()
+    }, [])
         
     const mobileNavOpen = () => {
-        symbol.classList.add('change');
-        symbol.children[1].classList.remove('spin');
-        mobileMenu.classList.add('deployed');
-        overlay.classList.add('active');
+        navDeployment.current.classList.add('change')
         body.style.overflowY = 'hidden';
         body.parentElement.style.overflowY = 'hidden';
-        hideMobileNav()
     }
 
     const mobileNavClose = () => {
         setTimeout(function() {
-            symbol.classList.remove('change')
+            navDeployment.current.classList.remove('change')
         }, 850);
-        symbol.children[1].classList.add('spin');
-        mobileMenu.classList.remove('deployed');
-        mobileMenu.style.transitionDelay = '450ms';
-        overlay.classList.remove('active');
-        overlay.style.transitionDelay = '450ms';
         body.style.overflowY = 'visible';
         body.parentElement.style.overflowY = 'visible';
     }
 
-    const hideMobileNav = () => {
-        let allMobileMenuItems = [...mobileMenu.children[0].children].concat(mobileMenu.children[1]);
+    const handleMobileNavClick = () => {
+        let menu = document.querySelector('.navbar__mobile__menu'),
+            menuChildren = [...menu.children[0].children].concat(menu.children[1]);
 
-        allMobileMenuItems.forEach(link => link.addEventListener('click', () => {
-            mobileNavClose()
-        }))
+        menuChildren.forEach(child => child.addEventListener('click', () => {
+            setMenuOpen(false);
+            mobileNavClose();
+        }));
     }
 
     return (
         <div className="navbar">
-            <Link to="/">
+            <Link to="/" onClick={() => setMenuOpen(false)}>
                 <img className="navbar__logo" src={logo} alt="Photosnap company logo" />
             </Link>
             <div className="navbar__links">
                 <PageLinks />
             </div>
             <Button text="Get an invite" />
-            <a href="#" className="navbar__mobile__btn" aria-label="Button for mobile menu deployment">
-                <img className="navbar__mobile__btn--open" src={open} onClick={mobileNavOpen} alt="" />
-                <img className="navbar__mobile__btn--close" src={close} onClick={mobileNavClose} alt="" />
+            <a href="#" className="navbar__mobile__btn" ref={navDeployment} aria-label="Button for mobile menu deployment" onClick={() => setMenuOpen(!menuOpen)}>
+                <img 
+                    className="navbar__mobile__btn--open" 
+                    src={open} 
+                    onClick={mobileNavOpen} 
+                    alt="" 
+                />
+                <img 
+                    className={`navbar__mobile__btn--close ${!menuOpen ? 'spin' : ''}`} 
+                    src={close} 
+                    onClick={mobileNavClose}
+                    alt="" 
+                />
             </a>
-            <div className="navbar__mobile__menu" onClick={hideMobileNav}>
+            <div className={`navbar__mobile__menu ${menuOpen ? 'deployed' : ''}`} onClick={handleMobileNavClick}>
                 <PageLinks />
                 <Button text="Get an invite" />
             </div>
-            <div className="overlay" />
+            <div className={`overlay ${menuOpen ? 'active' : ''}`} />
         </div>
     )
 }
