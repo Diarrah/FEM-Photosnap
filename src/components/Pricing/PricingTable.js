325c3d7f5f7e50364749fd56e319650dee7be7f3
Diarrah <diarrahcasiano@gmail.com>
2020-09-17 21:56:33 -0700

    Creating array of features + mapping each into table body row

--- src/components/Pricing/PricingTable.js
+++ src/components/Pricing/PricingTable.js
@@ -1,83 +1,42 @@
 import React from 'react'
 
-const PricingTable = () => (
-    <div className="pricing__table__container">
-        <h2 className="pricing__table__heading">Compare</h2>
-        <div className="pricing__table">
-            <div className="pricing__table__row">
-                <p className="pricing__table__row--feature">The Features</p>
-                <ul className="pricing__table__row--plans">
-                    <li className="pricing__table__row--plans-basic">Basic</li>
-                    <li className="pricing__table__row--plans-pro">Pro</li>
-                    <li className="pricing__table__row--plans-basic">Business</li>
-                </ul>
-            </div>
-            <div className="pricing__table__row">
-                <p className="pricing__table__row--feature">Unlimited Story Posting</p>
-                <ul className="pricing__table__row--available">
-                    <li className="checked" />
-                    <li className="checked" />
-                    <li className="checked" />
-                </ul>
-            </div>
-            <div className="pricing__table__row">
-                <p className="pricing__table__row--feature">Unlimited Photo Upload</p>
-                <ul className="pricing__table__row--available">
-                    <li className="checked" />
-                    <li className="checked" />
-                    <li className="checked" />
-                </ul>
-            </div>
-            <div className="pricing__table__row">
-                <p className="pricing__table__row--feature">Embedding Custom Content</p>
-                <ul className="pricing__table__row--available">
-                    <li />
-                    <li className="checked" />
-                    <li className="checked" />
-                </ul>
-            </div>
-            <div className="pricing__table__row">
-                <p className="pricing__table__row--feature">Customize Metadata</p>
-                <ul className="pricing__table__row--available">
-                    <li />
-                    <li className="checked" />
-                    <li className="checked" />
-                </ul>
-            </div>
-            <div className="pricing__table__row">
-                <p className="pricing__table__row--feature">Advanced Metrics</p>
-                <ul className="pricing__table__row--available">
-                    <li />
-                    <li />
-                    <li className="checked" />
-                </ul>
-            </div>
-            <div className="pricing__table__row">
-                <p className="pricing__table__row--feature">Photo Downloads</p>
-                <ul className="pricing__table__row--available">
-                    <li />
-                    <li />
-                    <li className="checked" />
-                </ul>
-            </div>
-            <div className="pricing__table__row">
-                <p className="pricing__table__row--feature">Search Engine Indexing</p>
-                <ul className="pricing__table__row--available">
-                    <li />
-                    <li />
-                    <li className="checked" />
-                </ul>
-            </div>
-            <div className="pricing__table__row">
-                <p className="pricing__table__row--feature">Custom Analytics</p>
-                <ul className="pricing__table__row--available">
-                    <li />
-                    <li />
-                    <li className="checked" />
-                </ul>
+const PricingTable = () => {
+    const features = [
+        {name: 'Unlimited Story Posting', basic: true, pro: true, business: true },
+        {name: 'Unlimited Photo Upload', basic: true, pro: true, business: true },
+        {name: 'Embedding Custom Content', basic: false, pro: true, business: true },
+        {name: 'Customize Metadata', basic: false, pro: true, business: true },
+        {name: 'Advanced Metrics', basic: false, pro: false, business: true },
+        {name: 'Photo Downloads', basic: false, pro: false, business: true },
+        {name: 'Search Engine Indexing', basic: false, pro: false, business: true },
+        {name: 'Custom Analytics', basic: false, pro: false, business: true }
+    ];
+
+    return (
+        <div className="pricing__table__container">
+            <h2 className="pricing__table__heading">Compare</h2>
+            <div className="pricing__table">
+                <div className="pricing__table__row table__header">
+                    <p className="pricing__table__row--feature">The Features</p>
+                    <ul className="pricing__table__row--plans">
+                        <li className="pricing__table__row--plans-basic">Basic</li>
+                        <li className="pricing__table__row--plans-pro">Pro</li>
+                        <li className="pricing__table__row--plans-basic">Business</li>
+                    </ul>
+                </div>
+                {features.map(({name, basic, pro, business}) => (
+                    <div className="pricing__table__row table__body" key={name}>
+                        <p className="pricing__table__row--feature">{name}</p>
+                        <ul className="pricing__table__row--available">
+                            <li className={basic ? 'checked' : ''} />
+                            <li className={pro ? 'checked' : ''}  />
+                            <li className={business ? 'checked' : ''} />
+                        </ul>
+                    </div>
+                ))}               
             </div>
         </div>
-    </div>
-)
+    )
+}
 
 export default PricingTable
